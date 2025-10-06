51dbdb93630f95637c27c7025139bfee63434fea
unknown <deepak0103gupta@gmail.com>
2025-10-06 14:42:15 +0530

    Updated CV

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
                                 Hire Me
                         </button>
                     </a>
-                    <a href="/cv.pdf" download="cv" className="flex flex-col  justify-center gap-4">
+                    <a href="/Deepak_CV.pdf" download="cv" className="flex flex-col  justify-center gap-4">
                         <button  class="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
                                 Download CV
                         </button>                       
